c203dda7ee39528e27144c18d1c2a33ec63b5a26
Łukasz Kostka <lukasz.kostka@netng.pl>
2015-12-08 23:40:11 +0100

    Rewritten plugin to count elapsed time at the end.

--- callbacks/profiler.py
+++ callbacks/profiler.py
@@ -1,56 +1,64 @@
 # -*- coding: UTF-8 -*-
 """Add task and playbook profiling."""
 
-import time
+from time import time as timer
+from collections import namedtuple
 from ansible.utils import display
+
+StartedTask = namedtuple('StartedTask', ('name', 'time'))
 
 
 class CallbackModule(object):
 
-    """Add task and playbook profiling."""
+    """Task and playbook profiling."""
+
+    # How many tasks to display at the end.
+    top_tasks = 10
 
     def __init__(self):
         """Record playbook start time."""
-        self.start_time = time.time()
-        self.end_time = None
-        self.stats = {}
-        self.current_task = None
-        self.top_n = 10
+        self.playbook_start_time = None
+        self.playbook_end_time = None
+        self.recorded_tasks = list()
 
-    def _record_current_task(self, name):
-        """Log start of task."""
-        self._record_last_task()
-        # Record the start time of the current task
-        self.current_task = name
-        self.stats[self.current_task] = time.time()
+    def _record_task(self, name):
+        """Record current task start time."""
+        self.recorded_tasks.append(StartedTask(name=name, time=timer()))
 
-    def _record_last_task(self):
-        """Record the running time of the last executed task."""
-        if self.current_task is not None:
-            self.stats[self.current_task] = time.time() - self.stats[self.current_task]
+    def playbook_on_start(self):
+        """Excuted when playbook starts."""
+        self.playbook_start_time = timer()
+
+    def v2_playbook_on_start(self, playbook):
+        """Excuted when playbook starts."""
+        self.playbook_on_start()
 
     def playbook_on_task_start(self, name, is_conditional):
         """Executed when task starts."""
-        self._record_current_task(name)
+        self._record_task(name)
 
     def v2_playbook_on_handler_task_start(self, task):
         """Executed when handler starts."""
-        self._record_current_task('HANDLER: ' + task.name)
+        self._record_task('HANDLER: ' + task.name)
+
+    @property
+    def elapsed(self):
+        """Count elapsed time for tasks."""
+        start_tasks = self.recorded_tasks
+        next_tasks = self.recorded_tasks[1:] + [StartedTask(name='PLAYBOOK END', time=self.playbook_end_time)]
+        return (
+                StartedTask(name=start_task.name, time=(next_task.time - start_task.time))
+                for start_task, next_task in zip(start_tasks, next_tasks)
+                )
 
     @property
     def results(self):
-        """Sort tasks by their running time. Longest first."""
+        """Sort tasks by their runtime. Longest first."""
         return sorted(
-            self.stats.items(),
-            key=lambda value: value[1],
+            self.elapsed,
+            key=lambda task: task.time,
             reverse=True,
         )
-
-    @property
-    def playbook_runtime(self): # noqa
-        if not self.end_time:
-            self.end_time = time.time()
-        return self.end_time - self.start_time
 
     @staticmethod
     def format_time(hours, minutes, seconds):
@@ -79,12 +87,12 @@ class CallbackModule(object):
 
     def display_results(self):
         """Display top n task results."""
-        display('Top {} tasks:'.format(self.top_n))
-        for name, elapsed in self.results[:self.top_n]:
+        display('Top {} tasks:'.format(self.top_tasks))
+        for name, elapsed in self.results[:self.top_tasks]:
             self.display_result(name, elapsed)
 
     def playbook_on_stats(self, stats):
         """Executed on PLAY RECAP."""
-        self._record_last_task()
-        self.display_result('Playbook runtime', self.playbook_runtime)
+        self.playbook_end_time = timer()
+        self.display_result('Playbook runtime', (self.playbook_end_time - self.playbook_start_time))
         self.display_results()
